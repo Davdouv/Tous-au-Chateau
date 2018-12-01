d352ef643dc928ca60e0d2bbb4a9c329cdacc71c
justine vuillemot <vuillemotjustine@gmail.com>
2018-12-01 18:51:00 +0100

    first test for UI display and resource manager file

---
+++ Tous-au-Chateau-Project/Assets/IUManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IUManager : MonoBehaviour {
+
+    public Text woodTxt;
+    public Text stoneTxt;
+    public Text foodTxt;
+    public Text villagersTxt;
+    public Slider motivation;
+
+    public ResourceManager resourceManager;
+
+	void Start () {
+        woodTxt.text = "Wood : " + resourceManager.GetWood();
+        stoneTxt.text = "Stone : " + resourceManager.GetStone();
+        foodTxt.text = "Food : " + resourceManager.GetFood();
+        villagersTxt.text = "Villagers : " + resourceManager.GetVillagers();
+        motivation.value = resourceManager.GetMotivation();
+    }
+
+    private void Update()
+    {
+        woodTxt.text = "Wood : " + resourceManager.GetWood();
+        stoneTxt.text = "Stone : " + resourceManager.GetStone();
+        foodTxt.text = "Food : " + resourceManager.GetFood();
+        villagersTxt.text = "Villagers : " + resourceManager.GetVillagers();
+        motivation.value = resourceManager.GetMotivation();
+    }
+
+    /* WOOD */
+    public void IncreaseWood()
+    {
+        resourceManager.SetWood(resourceManager.GetWood() + 10);
+    }
+
+    public void DecreaseWood()
+    {
+        resourceManager.SetWood(resourceManager.GetWood() - 5);
+    }
+
+    /* STONE */
+    public void IncreaseStone()
+    {
+        resourceManager.SetStone(resourceManager.GetStone() + 10);
+    }
+
+    public void DecreaseStone()
+    {
+        resourceManager.SetStone(resourceManager.GetStone() - 10);
+    }
+
+    /* FOOD */
+    public void IncreaseFood()
+    {
+        resourceManager.SetFood(resourceManager.GetFood() + 5);
+    }
+
+    public void DecreaseFood()
+    {
+        resourceManager.SetFood(resourceManager.GetFood() - 5);
+    }
+
+    /* VILLAGERS */
+    public void IncreaseVillagers()
+    {
+        resourceManager.SetVillagers(resourceManager.GetVillagers() + 3);
+    }
+
+    public void DecreaseVillagers()
+    {
+        resourceManager.SetVillagers(resourceManager.GetVillagers() - 1);
+    }
+
+    /* MOTIVATION */
+    public void IncreaseMotivation()
+    {
+        resourceManager.SetMotivation(resourceManager.GetMotivation() + 10);
+    }
+
+    public void DecreaseMotivation()
+    {
+        resourceManager.SetMotivation(resourceManager.GetMotivation() - 10);
+    }
+}
---
+++ Tous-au-Chateau-Project/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+
+    private int woodNb = 0;
+    private int stoneNb = 0;
+    private int foodNb = 0;
+    private int villagersNb = 30;
+    private float motivation = 100; //% => value from 0 to 100
+
+    /*GETTERS*/
+    public int GetWood()
+    {
+        return woodNb;
+    }
+
+    public int GetStone()
+    {
+        return stoneNb;
+    }
+
+    public int GetFood()
+    {
+        return foodNb;
+    }
+
+    public int GetVillagers()
+    {
+        return villagersNb;
+    }
+
+    public float GetMotivation()
+    {
+        return motivation;
+    }
+
+    /*SETTERS*/
+    public void SetWood(int wood)
+    {
+        if (wood >= 0)
+        {
+            woodNb = wood;
+        }
+    }
+
+    public void SetStone(int stone)
+    {
+        if(stone >= 0)
+        {
+            stoneNb = stone;
+        }
+    }
+
+    public void SetFood(int food)
+    {
+        if(food >= 0)
+        {
+            foodNb = food;
+        }
+    }
+
+    public void SetVillagers(int villagers)
+    {
+        if(villagers >= 0)
+        {
+            villagersNb = villagers;
+        }
+    }
+
+    public void SetMotivation(float newMotivation)
+    {
+        //% => value from 0 to 100
+        if (newMotivation >= 0 && newMotivation <= 100)
+        {
+            motivation = newMotivation;
+        }
+    }
+}
