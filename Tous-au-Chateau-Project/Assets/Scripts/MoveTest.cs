ae81a540ab6d3a46fefed5ebf2497852e54c880e
Davdouv <d4vid.n@hotmail.fr>
2018-12-02 17:38:49 +0100

    Add script for target detection
    You need to assign Tags to see the script working

---
+++ Tous-au-Chateau-Project/Assets/Scripts/MoveTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour {
+
+    float speed = 10f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Basic 2D movements for test
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0));
+        transform.Translate(new Vector3(0, Input.GetAxis("Vertical") * Time.deltaTime * speed, 0));
+    }
+}
