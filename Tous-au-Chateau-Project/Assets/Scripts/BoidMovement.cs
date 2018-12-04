a4c490fefb3d99ea1e7c28828cf462ac2b9a3478
Kenthyvong Tan <kenthyvong.tan@gmail.com>
2018-12-04 16:30:23 +0100

    Adding Basic movement, collision and death conditions

---
+++ Tous-au-Chateau-Project/Assets/Scripts/BoidMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+Boid Movement
+Déplace le gameobject en ligne droite.
+Nécessite :
+	- un gameobject nommé "Objectif" comme direction initiale
+
+*/
+public class BoidMovement : MonoBehaviour {
+
+	public float speed = 1.50f;
+	
+
+	// Use this for initialization
+	void Start () {
+		transform.LookAt(GameObject.Find("Objectif").transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// movement
+		transform.position += speed * transform.forward * Time.deltaTime;
+		
+			
+	}
+			
+}
