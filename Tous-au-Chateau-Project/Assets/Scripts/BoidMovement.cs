efba193eff1b934fc9d1f31f8d0f4f0bbdf24c65
Kenthyvong Tan <kenthyvong.tan@gmail.com>
2018-12-15 11:26:48 +0100

    Villagers basic straight-forward movement, including wallsliding

--- Tous-au-Chateau-Project/Assets/Scripts/BoidMovement.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/BoidMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 /*
@@ -8,24 +9,44 @@ Boid Movement
 Déplace le gameobject en ligne droite.
 Nécessite :
 	- un gameobject nommé "Objectif" comme direction initiale
+    - un Rigidbody:
+        kinematic = true,
+        gravity = false (?), 
+        interpolate = none, 
+        collision detection = continuous
+    - Une surface avec un component Navmesh Surface sur lequel se déplacer
+    - un component Navmesh Agent
+autre : des obstacles avec Navmesh Obstacle
+
 
 */
-public class BoidMovement : MonoBehaviour {
+public class BoidMovement : MonoBehaviour
+{
 
-	public float speed = 1.50f;
-	
+    public float speed = 1.50f;
+    Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-		transform.LookAt(GameObject.Find("Objectif").transform.position);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// movement
-		transform.position += speed * transform.forward * Time.deltaTime;
-		
-			
-	}
-			
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.LookAt(GameObject.Find("Objectif").transform.position);
+        rb.freezeRotation = true;
+        rb.isKinematic = true;
+    }
+
+    // FixedUpdate est conseillé en cas d'instructions sur la physique
+    void FixedUpdate()
+    {
+        // movement
+        rb.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+
+        //transform.position += speed * transform.forward * Time.deltaTime;
+        //transform.Translate(transform.forward * Time.deltaTime);
+        //rb.AddForce(transform.forward * Time.fixedDeltaTime, ForceMode.Force);
+        //rb.position = transform.position + transform.forward * speed * Time.fixedDeltaTime;
+        //print(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+        //rb.velocity = transform.forward * speed * Time.fixedDeltaTime;
+    }
+
 }
