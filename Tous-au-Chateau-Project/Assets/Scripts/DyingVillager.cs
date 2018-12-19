61953dd12e0c96d964225f7e02f06172d4eaf358
AmelN <amel.negra@gmail.com>
2018-12-19 10:17:26 +0100

    good scale

--- Tous-au-Chateau-Project/Assets/Scripts/DyingVillager.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/DyingVillager.cs
@@ -6,12 +6,12 @@ public class DyingVillager : MonoBehaviour {
     public bool isAlive = true;
     public float turningRate = 20f; // maximum turn rate in degrees per second
     private Quaternion _targetRotation = Quaternion.identity;
-    public ParticleSystem ghost; // assigned in editor
+    //public ParticleSystem ghost; // assigned in editor
     public GameObject mesh; // assigned in editor
    
     // Use this for initialization
     void Start () {
-        ghost.Pause();
+        //ghost.Pause();
     }
 	
 	// Update is called once per frame
@@ -21,7 +21,7 @@ public class DyingVillager : MonoBehaviour {
             // Turn towards our target rotation.
             SetBlendedEulerAngles(new Vector3(90, 0, 0)); // rotation on x axis 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, 4);
-            ghost.Play();
+            //ghost.Play();
         }
 	}
 
