a1faac4f2aadd5d29613156e805b3eee24773413
Davdouv <d4vid.n@hotmail.fr>
2019-01-06 21:17:10 +0100

    Add a Target class to use it inside TriggerZone class to fix some issues

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Target.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target {
+
+    private GameObject _targetObject;
+    private bool _isTriggered;
+
+    public Target(GameObject target)
+    {
+        _targetObject = target;
+        _isTriggered = false;
+    }
+
+    public void Trigger()
+    {
+        _isTriggered = true;
+    }
+
+    public bool IsTriggered()
+    {
+        return _isTriggered;
+    }
+
+    public GameObject GetObject()
+    {
+        return _targetObject;
+    }
+}
