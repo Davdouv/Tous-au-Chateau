6133578e26c43beed2efea8694bd024d890f89ae
AlexaneLG <leguern.a@laposte.net>
2019-01-07 03:40:16 +0100

    Add ChangeHeightMap script

---
+++ Tous-au-Chateau-Project/Assets/Scripts/ChangeHeightMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHeightMap : MonoBehaviour {
+
+    public Terrain terrain;
+    public GameObject map;
+
+    public void Increase()
+    {
+        terrain.transform.position = new Vector3(terrain.transform.position.x, terrain.transform.position.y + 0.01f, terrain.transform.position.z);
+        map.transform.position = new Vector3(map.transform.position.x, map.transform.position.y + 0.01f, map.transform.position.z);
+    }
+
+    public void Decrease()
+    {
+        terrain.transform.position = new Vector3(terrain.transform.position.x, terrain.transform.position.y - 0.01f, terrain.transform.position.z);
+        map.transform.position = new Vector3(map.transform.position.x, map.transform.position.y - 0.01f, map.transform.position.z);
+    }
+}
