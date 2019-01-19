6ebd5e46610f5f9bb8b35e9c2e47713f6d4c5dea
AmelN <amel.negra@gmail.com>
2019-01-19 17:04:00 +0100

    adding construction

---
+++ Tous-au-Chateau-Project/Assets/Scripts/UI/Building.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Building {
+
+    private RessourcesPack _cost;
+    private RessourcesPack _provision;
+    private float _effectArea; //the radius of effect area
+    private float _width;
+    private float _height;
+    private bool _isDraggable;
+
+    public void crush()
+    {
+
+    }
+    public bool canBuy()
+    {
+        return;
+    }
+
+    public bool ApplyEffect()
+    {
+        return;
+    }
+
+    public bool Drag()
+    {
+        return;
+    }
+
+}
---
+++ Tous-au-Chateau-Project/Assets/Scripts/UI/WoodPlank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : Building {
+
+    public void crush()
+    {
+        if(this.gameObject.CompareTag("bridge") == "")
+        {
+
+        }
+    }
+    public bool canBuy()
+    {
+        if (this.gameObject.CompareTag("bridge") == "")
+        {
+
+        }
+    }
+
+    public bool ApplyEffect()
+    {
+        return;
+    }
+
+}
