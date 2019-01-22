7e9c3e0dee6a57c0cdd061e8beab0a464cf37820
Davdouv <d4vid.n@hotmail.fr>
2019-01-22 17:48:54 +0100

    Create script & test scene

---
+++ Tous-au-Chateau-Project/Assets/Scripts/DirectionnalPannelEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionnalPannelEvent : TriggerZone {
+
+    private bool _isOnTheGround;
+
+    public override void CollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isOnTheGround = true;
+        }        
+    }
+
+    public override void CollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isOnTheGround = false;
+        }
+    }
+
+    public override void TriggerEnter(GameObject target)
+    {
+        if (_isOnTheGround && target.tag != "Ground")
+        {
+            // TODO 
+            // --> Need to change BoidMovement to Villager
+            // --> Need to change "LeftWoodSign" to DirectionnalPannel.GetDirection
+            // --> Need to change Turn method in Villager
+            target.GetComponent<BoidMovement>().Turn("LeftWoodSign");
+        }        
+    }
+}
