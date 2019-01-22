75eb77b26278d752b12a34fc1bfd16bd323c7a0d
Etienne Juif <etienne.juif@gmail.com>
2019-01-22 22:10:28 +0100

    Add building to hand and release

--- Tous-au-Chateau-Project/Assets/actions.cs
+++ Tous-au-Chateau-Project/Assets/actions.cs
@@ -5,39 +5,68 @@ using UnityEngine;
 public class actions : MonoBehaviour {
 
     public ResourceManager resourceM;
+    public GameObject spawnPoint;
+    public GameObject bridge;
+    public GameObject wall;
 
     VRTK.VRTK_ControllerEvents events;
     bool trigger;
+    bool crushMode;
+    bool haveBuilding;
     Vector3 currentPos;
+
+    GameObject newBuilding;
 
     // Use this for initialization
     void Start () {
         events = GetComponent<VRTK.VRTK_ControllerEvents>();
+        trigger = false;
+        crushMode = false;
+        haveBuilding = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (events.triggerPressed)
         {
+            if (!trigger)
+            {
+                currentPos = gameObject.transform.position;
+            }
             trigger = true;
-            currentPos = gameObject.transform.position;
         }
         else
         {
             trigger = false;
+            crushMode = false;
+            if (haveBuilding)
+            {
+                //releaseBuilding
+                haveBuilding = false;
+                newBuilding.GetComponent<Rigidbody>().isKinematic = false;
+            }
         }
 
         if (trigger)
         {
-            currentPos = gameObject.transform.position;
-            Debug.Log(currentPos);
+            //currentPos = gameObject.transform.position;
+            if(gameObject.transform.position.y < currentPos.y - 15)
+            {
+                crushMode = true;
+            }
+        }
+
+        if (haveBuilding)
+        {
+            newBuilding.transform.position = spawnPoint.transform.position;
+            newBuilding.transform.localRotation = spawnPoint.transform.localRotation;
         }
 	}
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.tag);
-        if (trigger)
+        if (crushMode) //Destroy element of the nature
         {
             if (other.tag == "Wood")
             {
@@ -59,7 +88,33 @@ public class actions : MonoBehaviour {
                 Destroy(other.gameObject);
             }
         }
+
+        //For buildings
         
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (events.triggerPressed && !haveBuilding)
+        {
+            if (other.tag == "bridge")
+            {
+                if (resourceM.RemoveWood(5))
+                {
+                    newBuilding = Instantiate(bridge, spawnPoint.transform.position, new Quaternion(0, 0, 0, 0));
+                    haveBuilding = true;
+                }
+            }
+            else if (other.tag == "wall")
+            {
+                if (resourceM.RemoveStone(5))
+                {
+                    newBuilding = Instantiate(wall, spawnPoint.transform.position, new Quaternion(0, 0, 0, 0));
+                    haveBuilding = true;
+                }
+            }
+        }
     }
 
     //void OnTrigg
