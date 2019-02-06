78b4b664c6be246d7e8ef0cb006ba215d8bca9ac
AlexaneLG <leguern.a@laposte.net>
2019-02-06 13:39:01 +0100

    Add woodboard

--- Tous-au-Chateau-Project/Assets/Scripts/Victory.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/Victory.cs
@@ -10,15 +10,16 @@ public class Victory : TriggerZone {
     public List<GameObject> villagers = new List<GameObject>();
     public float speed;
     private float _step;
+    private bool _isNearCastle = false;
     private bool _isEscaping = false;
-    private bool _hasEscaped = false;
+    private bool _hasEscaped = false; // use this to know if the lord escaped
     
     public override void TriggerEnter(GameObject villager)
     {
         Debug.Log("VICTORY ! A villager is in the castle !");
         villagers.Add(villager);
-        // evil lord escapes
-        _isEscaping = true;
+        _isNearCastle = true; // a villager reach the castle
+        _isEscaping = true; // evil lord escapes
     }
 
     protected override void Update()
@@ -28,22 +29,25 @@ public class Victory : TriggerZone {
         _step = speed * Time.deltaTime;
 
         // if a villager reachs the castle
-        if (_isEscaping)
+        if (_isNearCastle)
         {
-            // evil lord escapes
-            evilLord.transform.position = Vector3.MoveTowards(evilLord.transform.position, escapeDestination.transform.position, _step);
-
-            // villagers walk inside the castle
             foreach (GameObject villager in villagers)
             {
-                villager.transform.position = Vector3.MoveTowards(villager.transform.position, castleDestination.transform.position, _step);
+                villager.transform.position = Vector3.MoveTowards(villager.transform.position, castleDestination.transform.position, _step); // villagers walk inside the castle
             }
+        }
+
+        if (_isEscaping)
+        {
+            evilLord.transform.position = Vector3.MoveTowards(evilLord.transform.position, escapeDestination.transform.position, _step); // evil lord escapes
+            evilLord.transform.LookAt(escapeDestination.transform);
         }
 
         if (Vector3.Distance(evilLord.transform.position, escapeDestination.transform.position) < 2)
         {
             _hasEscaped = true;
             Debug.Log("The evil lord has escaped :'(");
+            _isEscaping = false;
         }
     }
 }
