7dd70be11988360a57581caa936f25d6424dc00b
Davdouv <d4vid.n@hotmail.fr>
2019-02-06 14:53:14 +0100

    Clean GatherDetection class

--- Tous-au-Chateau-Project/Assets/Scripts/GatherDetection.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/GatherDetection.cs
@@ -3,43 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GatherDetection : TriggerZone {
-
-    public int _radius= 5;
-
-
-    // Use this for initialization
-    void Start()
-    {
-        distanceDetection = _radius;
-        SphereCollider reach = gameObject.AddComponent<SphereCollider>();
-        reach.radius = _radius;
-        reach.isTrigger = true;
-        targetTag.Add("Villager");
-    }
-
-
+    
     // On Detection, 
     public override void TriggerEnter(GameObject target)
     {
-        
-        // 
-        if (target.tag == "Villager")
+        if (target.GetComponent<Villager>().IsPassive() && !GetComponent<Villager>().IsPassive())
         {
-            if (target.GetComponent<Villager>().IsPassive() && !GetComponent<Villager>().IsPassive())
-            {
-                Debug.Log(name + " GATHERING TRIGGER ENTER : " + target.name);
-                CallOut(target);
-            }
+            Debug.Log(name + " GATHERING TRIGGER ENTER : " + target.name);
+            CallOut(target);
         }
-
     }
-    
 
     private void CallOut(GameObject target)
     {
         target.GetComponent<Villager>().JoinIn(gameObject);
 
     }
-    
-
 }
