25d4c35b10692de9363951c9a9ba2687c38dbba5
Damagae <daphnegm.rose0gmail.com>
2019-02-06 15:01:37 +0100

    Speech system imported

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Speech Events/SpeechEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechEvent : MonoBehaviour {
+
+	[TextArea(3,10)]
+	public string message;
+	public SpeechBubble bubble;
+	private bool isOpen = false;
+	public bool ouvrir = false;
+
+	// Use this for initialization
+	void Start () {
+		bubble.setMessage(message);
+	}
+
+	public bool CanOpen() { return ouvrir; }
+
+	public bool CanClose() { return !ouvrir; }
+
+	public bool IsOpen() { return isOpen; }
+	public void SetIsOpen(bool bo) { isOpen = bo; }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Tous-au-Chateau-Project/Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechManager : MonoBehaviour {
+
+	public bool showMessage;
+	public SpeechBubble sb;
+	public SpeechEvent[] events;
+	private bool _previousState;
+	private Animator _anim;
+	private bool _canDisable = false;
+
+	//Screen to open automatically at the start of the Scene
+  // public Animator initiallyOpen;
+
+  //Hash of the parameter we use to control the transitions.
+  private int _openParameterId;
+
+	//Animator State and Transition names we need to check against.
+  const string _openTransitionName = "Open";
+  const string _closedStateName = "Closed";
+
+  //The GameObject Selected before we opened the current Screen.
+  //Used when closing a Screen, so we can go back to the button that opened it.
+  // private GameObject m_PreviouslySelected;
+
+	void Update() {
+		// if (_previousState != showMessage) {
+		// 	if (showMessage)
+		// 	{
+		// 		Open(sb);
+		// 	}
+		// 	else if (!showMessage)
+		// 	{
+		// 		Close(sb);
+		// 	}
+		// }
+		// if (!showMessage && _canDisable)
+		// 	sb.gameObject.SetActive(false);
+		//
+		// _previousState = showMessage;
+
+		foreach (var evento in events)
+		{
+			if (!evento.IsOpen() && evento.CanOpen())
+			{
+				evento.SetIsOpen(true);
+				Open(evento.bubble);
+			} else if (evento.IsOpen() && evento.CanClose())
+			{
+				evento.SetIsOpen(false);
+				Close(evento.bubble);
+			}
+		}
+	}
+
+  void Start()
+  {
+			_previousState = showMessage;
+			_anim = sb.GetComponent<Animator>();
+      //We cache the Hash to the "Open" Parameter, so we can feed to Animator.SetBool.
+      _openParameterId = Animator.StringToHash(_openTransitionName);
+			_anim.SetBool(_openTransitionName, true);
+  }
+
+	private void Open(SpeechBubble bubble) {
+		bubble.gameObject.SetActive(true);
+		_anim.SetBool(_openTransitionName, true);
+		bubble.StartAnimation();
+	}
+
+	private void Close(SpeechBubble bubble) {
+		if (bubble.CanClose())
+		{
+			_anim.SetBool(_openTransitionName, false);
+			StartCoroutine(DisablePanel(bubble));
+		} else {
+			Debug.Log("Animation is still playing.");
+			showMessage = true;
+		}
+	}
+
+  //Coroutine that will detect when the Closing _animation is finished and it will deactivate the
+  //hierarchy.
+  IEnumerator DisablePanel(SpeechBubble bubble)
+  {
+			var currentClipInfo = _anim.GetCurrentAnimatorClipInfo(0);
+			yield return new WaitForSeconds(3);
+			_canDisable = true;
+  }
+
+}
