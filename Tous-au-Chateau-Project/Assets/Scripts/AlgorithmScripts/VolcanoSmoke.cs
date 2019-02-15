10fd6736b18bf15a397344e24e333c1404c8130c
Amel <amel.negra@gmail.com>
2019-02-15 15:10:28 +0100

    volcano added

---
+++ Tous-au-Chateau-Project/Assets/Scripts/AlgorithmScripts/VolcanoSmoke.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class VolcanoSmoke : MonoBehaviour {
+
+    public Vector3 spawn;
+    public static float moveSpeed = 0.01f;
+    public static float maxSpeed = 1.0f;
+
+    public static int brainSize = 10;
+    public Vector3[] brain = new Vector3[brainSize];   //brain stores vectors of movement
+    public int i = 0;   //brain iterator
+    public int lifespan =  1;    //number of steps it can take before it dies (increased each 5 generations by population.cs script)
+
+    public bool reachedTheGoal = false;
+    public bool dead = false;
+
+    public Material alive;
+    public Material notAlive;
+    public Material champion;
+    public Shader alwaysOnTop;  //this shader makes champion visible over other players
+
+    public float fitness = 0;
+
+    private float distToGround;
+    public float distToGoalFromSpawn;
+
+    // Use this for initialization
+    void Start (){
+        GenerateVectors();
+        spawn = transform.position;
+        //distToGround = 117f;
+        distToGround = 19f;
+    }
+
+
+    // Unity method for physics update
+    void FixedUpdate(){
+
+        if(!dead)
+        {
+            if (!IsGrounded()) Die();  //they die when there's no ground under them (we are in the air so not really a ground, but compared to initial height position)
+
+            if (i >= brainSize || i >= lifespan) Die();
+        }
+
+    }
+
+
+    public void MovePlayer()
+    {
+        if (!dead)
+        {
+            GetComponent<Rigidbody>().AddForce(brain[i] * moveSpeed * 0.01f);
+            i++;
+ 
+        }
+    }
+
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Goal")
+        {
+            reachedTheGoal = true;
+            Die();
+        }
+    }
+
+
+    public void Respawn()
+    {
+        transform.position = spawn;
+        GetComponent<Rigidbody>().velocity = Vector3.zero; //stop movement in case it was falling while being dead or sth idk
+        i = 0;
+        dead = false;
+        reachedTheGoal = false;
+        GetComponent<Renderer>().material = alive;
+    }
+
+
+    public void Die()
+    {
+        dead = true;
+        GetComponent<Renderer>().material = notAlive;
+        GetComponent<Rigidbody>().velocity = Vector3.zero; //stop movement
+    }
+
+
+    public void SetAsChampion()
+    {
+        GetComponent<Renderer>().material = champion;
+        GetComponent<Renderer>().material.shader = alwaysOnTop;
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 15.0f);
+    }
+
+
+     private void GenerateVectors()
+    {
+       for (int j = 0; j < brainSize; j++)
+            brain[j] = new Vector3(Random.Range(-10, 11), 0, Random.Range(10, -11));
+    }
+
+}
---
+++ Tous-au-Chateau-Project/Assets/Scripts/OtherScripts/Menu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject GameMaster;
+    public Text GameSpeedDisplay;
+    public Text GenerationCounter;
+    public Text PlayerNumDisplay;
+    public Slider GameSpeedSlider;
+    public Slider PlayerNumSlider;
+    static float gameSpeed = 1.0f;  //static so it doesn't change when level restarts
+    public static int volcanoSmokeNum = 10;
+
+
+    void Start()
+    {
+        UpdatePopNumberDisplay(volcanoSmokeNum);
+        UpdateGameSpeedDisplay(gameSpeed);
+
+        GameSpeedSlider.value = gameSpeed;
+        PlayerNumSlider.value = volcanoSmokeNum;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        UpdateGenerationText();
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void ChangeGameSpeedValue(float newSpeed)
+    {
+        gameSpeed = newSpeed;
+    }
+
+    public void UpdateGameSpeedDisplay(float newSpeed)
+    {
+        GameSpeedDisplay.text = "x" + newSpeed.ToString();
+    }
+
+    public void UpdateGenerationText()
+    {
+        int generation = GameMaster.GetComponent<Population>().generation;
+        GenerationCounter.text = "Generation: " + generation.ToString();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ChangePopulationNumber(float n)
+    {
+        volcanoSmokeNum = (int)n;
+    }
+
+    public void UpdatePopNumberDisplay(float n)
+    {
+        PlayerNumDisplay.text = n.ToString();
+    }
+}
