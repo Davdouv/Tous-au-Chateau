f07e31c4f42ee18096dfe8bcbd12dedba8be93d5
justine vuillemot <vuillemotjustine@gmail.com>
2019-02-23 12:23:39 +0100

    tuto event 6 done and tuto event 6.1 almost done

--- Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Tuto 1/SpeechEvent_MapTuto1_Event6_1.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Tuto 1/SpeechEvent_MapTuto1_Event6_1.cs
@@ -6,20 +6,34 @@ using UnityEngine;
 
 public class SpeechEvent_MapTuto1_Event6_1 : SpeechEvent {
 
-	public override bool MustOpen() {
+    public ResourceManager _ResourceManager;
+
+    private int woodBeforeCrushingTree = 0;
+
+    public override bool MustOpen() {
 		// Open after previous event is done
 		if (_previousEvent != null && _previousEvent.IsDone()) {
-			// TODO
-			if (/*group dies == true*/false) {
-				return true;
-			}
-		}
+            //TODO
+            if (currentVillagersGroup.GetNumberOfVillagersAlive() == 0)
+            {
+                // Check if at least one has fallen in the void
+                if (currentVillagersGroup.IsDeathCausedBy(DeathReason.VOID))
+                {
+                    woodBeforeCrushingTree = _ResourceManager.GetWood();
+                    return true;
+                }
+            }
+        }
 		return false;
 	}
 
 	public override bool MustClose() {
-		// TODO
-		// Crushing a tree
-		return false;
+        // Crushing a tree (any tree)
+        if (_ResourceManager.GetWood() >= woodBeforeCrushingTree + 10)
+        {
+            return true;
+        }
+
+        return false;
 	}
 }
