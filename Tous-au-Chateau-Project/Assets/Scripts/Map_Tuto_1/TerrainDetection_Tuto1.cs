28103fe981c6acf7b1d1059fe3baa0d072ea5801
Davdouv <d4vid.n@hotmail.fr>
2019-02-24 17:33:49 +0100

    Add hard coded script for event 4

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Map_Tuto_1/TerrainDetection_Tuto1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainDetection_Tuto1 : MonoBehaviour {
+
+    public SpeechEvent_MapTuto1_Event4 speechEvent;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 10) // 10 : Building
+        {
+            collision.gameObject.GetComponent<Building>().SetHasLanded();
+            if (collision.gameObject.GetComponent<WoodPlank>())
+            {
+                speechEvent.hasWookPlankLanded = true;
+            }
+        }
+    }
+}
--- Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Tuto 1/SpeechEvent_MapTuto1_Event4.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Tuto 1/SpeechEvent_MapTuto1_Event4.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class SpeechEvent_MapTuto1_Event4 : SpeechEvent
 {
     // TODO --> Need to set the currentBuilding somewhere
-    public Building currentBuilding;
+    //public WoodPlank currentBuilding;
+    public bool hasWookPlankLanded = false;
 
 	public override bool MustOpen() {
 		// Open after previous event is done
@@ -18,14 +19,10 @@ public class SpeechEvent_MapTuto1_Event4 : SpeechEvent
 	}
 
 	public override bool MustClose() {
-		// TODO
-		// Opening the Construction Panel and placing the wood board
-		// (not necessary at the right place, could be anywhere)
-        if (currentBuilding.HasLanded())
-        {
-            return true;
-        }
-
-		return false;
+        // TODO
+        // Opening the Construction Panel and placing the wood board
+        // (not necessary at the right place, could be anywhere)
+        //if (currentBuilding.HasLanded())
+        return hasWookPlankLanded;
 	}
 }
