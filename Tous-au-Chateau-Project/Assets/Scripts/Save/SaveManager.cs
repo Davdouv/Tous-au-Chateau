3c72603f5b15b7613e06dbf68e828d33710c441d
Damagae <daphnegm.rose0gmail.com>
2019-02-25 11:20:54 +0100

    Static class SaveManager for saving

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Save/SaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public static class SaveManager {
+
+	public static void Save(LevelScore newScore) {
+		StreamReader reader = new StreamReader(Application.persistentDataPath + "/score.dat");
+		StreamWriter writer = new StreamWriter(Application.persistentDataPath + "/score.dat");
+
+		string existingJSON = reader.ReadToEnd();
+		reader.Close();
+
+		List<LevelScore> existingScores = JsonHelperList.FromJson<LevelScore>(existingJSON);
+
+		// Remove the exisiting score if it exists for the current level
+		foreach (var score in existingScores) {
+			if (score.levelName == newScore.levelName) {
+				existingScores.Remove(score);
+			}
+		}
+
+		// Add new score into the list
+		existingScores.Add(newScore);
+
+		string newData = JsonHelperList.ToJson(existingScores, true);
+		writer.WriteLine(newData); // Rewrite score
+		writer.Close();
+	}
+
+}
