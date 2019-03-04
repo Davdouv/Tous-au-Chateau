2b94c08a1cdabcee9b5a11fbe5e9b72b974e9e32
Davdouv <d4vid.n@hotmail.fr>
2019-03-04 17:09:03 +0100

    Count Villagers in the chateau

--- Tous-au-Chateau-Project/Assets/Scripts/Victory.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/Victory.cs
@@ -1,23 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Victory : TriggerZone {
 
+    private uint scoreCount = 0;
+    public Text score;
+
 	// Use this for initialization
 	void Start () {
-        //targetTag.Add("Villager");
     }
 
-    public override void TriggerEnter(GameObject target)
+    public override void CollisionEnter(Collision collision)
     {
-        target.GetComponent<Villager>().SetHasReachedObjectif();
-        target.transform.position = this.transform.position;
+        Debug.Log("YO");
+        collision.gameObject.GetComponent<Villager>().SetHasReachedObjectif();
+        collision.transform.position = this.transform.position;
+
+        UpdateScore();
 
         if (VillagersManager.Instance.HasLastVillagersReachedObjectif())
         {
             Debug.Log("VICTORY");
             GameManager.Instance.GameWon();
         }
+    }
+
+    private void UpdateScore()
+    {
+        ++scoreCount;
+        /*
+        if (scoreCount % 3 == 0)
+        {
+            scoreCount /= 3;
+        }
+        */
+        score.text = scoreCount.ToString();
     }
 }
