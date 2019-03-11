da1a62d181f0f1f0e322e9176517de08d235cf40
AlexaneLG <leguern.a@laposte.net>
2019-03-11 12:00:44 +0100

    Add fade animation

---
+++ Tous-au-Chateau-Project/Assets/Scripts/SceneManagement/SceneTransitions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour {
+
+	public static SceneTransitions Instance;
+    public Animator transitionAnim;
+
+	public string scene1Name;
+	public string scene2Name;
+
+	void Awake() {
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+
+		}
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            StartCoroutine(LoadScene("end", scene2Name));
+        }
+	}
+
+    IEnumerator LoadScene(string animName, string sceneName)
+    {
+        transitionAnim.SetTrigger(animName);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
