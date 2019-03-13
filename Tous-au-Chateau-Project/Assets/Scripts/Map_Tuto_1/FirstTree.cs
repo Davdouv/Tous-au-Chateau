a0c6bf3e77497b1247eeeaeb1e4ef2a63282c453
Davdouv <d4vid.n@hotmail.fr>
2019-03-13 17:19:22 +0100

    Oups...

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Map_Tuto_1/FirstTree.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTree : MonoBehaviour
+{
+    // Callback
+    private void OnDestroy()
+    {
+        MapManager.Instance.SetFirstTreeDestroyed();
+    }
+}
