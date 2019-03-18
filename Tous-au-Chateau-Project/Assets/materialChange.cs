282fa9fee390275ed92dc101285c9017b1da4c61
Davdouv <d4vid.n@hotmail.fr>
2019-03-18 23:48:38 +0100

    Preview is red if colliding

--- Tous-au-Chateau-Project/Assets/materialChange.cs
+++ Tous-au-Chateau-Project/Assets/materialChange.cs
@@ -9,6 +9,9 @@ public class materialChange : MonoBehaviour
     public Material transparentRed_mat;
     Renderer rend;
     public bool inCollision;
+    private bool _mustChange = false;
+
+    protected List<GameObject> _targetList = new List<GameObject>();
 
     void Start()
     {
@@ -18,32 +21,111 @@ public class materialChange : MonoBehaviour
 
     void Update()
     {
-        if (inCollision)
+        if (_mustChange)
         {
-            //rend.material = transparentRed_mat;
-            ChangeMaterial(transparentRed_mat);
-        }
-        else
-        {
-            //rend.material = transparent_mat;
-            ChangeMaterial(transparent_mat);
+            if (inCollision)
+            {
+                //rend.material = transparentRed_mat;
+                ChangeMaterial(transparentRed_mat);
+            }
+            else
+            {
+                //rend.material = transparent_mat;
+                ChangeMaterial(transparent_mat);
+            }
         }
     }
 
+    // ***** DETECTION *****/
+    // Trigger must be enabled on collider
     private void OnTriggerEnter(Collider other)
     {
-        //if () {  add distance check
-            inCollision = true;
-        /*}
-        else
-        {
-            InCollision = false;
-        }*/
+        // Store the targets
+        _targetList.Add(other.gameObject);
+        Debug.Log("Add : " + other.gameObject.name);
     }
     private void OnTriggerExit(Collider other)
     {
-        //if () {  add distance check
-        inCollision = false;
+        // Remove the targets
+        RemoveTarget(other.gameObject);
+    }
+    protected void RemoveTarget(GameObject target)
+    {
+        foreach (GameObject tar in _targetList)
+        {
+            if (tar == target)
+            {
+                Debug.Log("Remove : " + target.name);
+                _targetList.Remove(tar);
+                return;
+            }
+        }
+    }
+    // Check distance between this & a target
+    private bool IsInRange(Vector3 position, float distanceDetection = 1f)
+    {
+        Vector3 closestPoint = GetComponent<BoxCollider>().ClosestPoint(position);
+        float distance = (closestPoint - position).sqrMagnitude;
+        return (distance < distanceDetection); // Detect if the given position is inside the sphere collider
+    }
+
+    // Check distance with all targets & test if we have to change material
+    private void FixedUpdate()
+    {
+        bool wasInCollision = inCollision;
+        bool inContact = false;
+        for (int i = 0; i < _targetList.Count && !inContact; ++i)
+        {
+            if (_targetList[i] != null)
+            {
+                // If it's the terrain, it's in contact
+                if (_targetList[i].tag == "Ground")
+                {
+                    inContact = true;
+                    break;
+                }
+                BoxCollider box = _targetList[i].GetComponent<BoxCollider>();
+                if (box)
+                {
+                    // Check distance with closest point
+                    if (IsInRange(box.ClosestPoint(transform.position)))
+                    {
+                        inContact = true;
+                    }
+                }
+                else
+                {
+                    // Check distance with position
+                    if (IsInRange(_targetList[i].transform.position))
+                    {
+                        inContact = true;
+                    }
+                }
+            }
+        }
+        inCollision = inContact;
+        if (wasInCollision != inCollision)
+        {
+            _mustChange = true;
+        }
+        else
+        {
+            _mustChange = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("TOTOTO");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (IsInRange(other.transform.position))
+        {
+            inCollision = true;
+        }
         /*}
         else
         {
@@ -53,6 +135,7 @@ public class materialChange : MonoBehaviour
 
     void ChangeMaterial(Material newMat)
     {
+        Debug.Log("Change Material");
         Renderer[] children;
         children = GetComponentsInChildren<Renderer>();
         foreach (Renderer rend in children)
