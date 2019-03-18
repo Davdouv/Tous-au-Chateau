56253574a32d0395b692d09ebf83c09e854577aa
Thyvong <kenthyvong.tan@gmail.com>
2019-03-18 12:12:45 +0100

    Press button to launch credits

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Map Selector/CreditAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditAnimation : MonoBehaviour {
+
+    public Transform player;
+	public void Launch()
+    {
+        GetComponent<Animator>().enabled = true ;
+    }
+    public void Stop()
+    {
+        GetComponent<Animator>().enabled = false;
+        gameObject.SetActive(false);
+
+    }
+    private void Update()
+    {
+        
+        transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+    }
+}
