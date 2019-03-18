0cd12a38e52730b4998e61759d69ed57d7dd467f
Thyvong <kenthyvong.tan@gmail.com>
2019-03-18 14:57:44 +0100

    buttons

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Map Selector/CreditButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditButtonScript : MonoBehaviour
+{
+    public Transform credits; // credits objects
+    
+    public void OnCollisionEnter(Collision collision)
+    {
+        Launch();
+        credits.gameObject.SetActive(true);
+        credits.GetComponent<CreditAnimation>().Launch();
+    }
+    public void Launch()
+    {
+        GetComponent<Animator>().enabled = true;
+    }
+    public void Stop()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+
+
+}
---
+++ Tous-au-Chateau-Project/Assets/Scripts/Map Selector/ExitButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonScript : MonoBehaviour {
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Animator>().enabled = true;
+        StartCoroutine(End());
+
+    }
+    public IEnumerator End()
+    {
+        yield return new WaitForSeconds(5);
+        Application.Quit();
+    }
+}
