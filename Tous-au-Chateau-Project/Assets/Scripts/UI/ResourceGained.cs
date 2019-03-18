c4304fd4fa72bb525b48ea276dd2072f2d446a19
justine vuillemot <vuillemotjustine@gmail.com>
2019-03-18 15:55:34 +0100

    added icon above crushed resource that indicate amount

---
+++ Tous-au-Chateau-Project/Assets/Scripts/UI/ResourceGained.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceGained : MonoBehaviour {
+
+    public float animationTime = 1.0f;
+
+    private Text _gainText;
+    private float _startTime;
+
+    private Vector3 _startPos;
+
+	// Use this for initialization
+	void Start () {
+        _gainText = transform.GetChild(0).GetComponent<Text>();
+        _startTime = Time.time;
+        _startPos = _gainText.rectTransform.position;
+
+        Invoke("SelfDestroy", animationTime + 1.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float timeElapsed = (Time.time - _startTime);
+        float fracJourney = timeElapsed / animationTime;
+
+        //Position
+        Vector3 self = _gainText.rectTransform.position;
+        self.y = Mathf.Lerp(_startPos.y, _startPos.y + 5.0f, fracJourney);
+        _gainText.rectTransform.position = self;
+
+        //Text alpha
+        Color c = _gainText.color;
+        c.a = Mathf.Lerp(0, 1, fracJourney);
+        _gainText.color = c;
+    }
+
+    private void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    public void UpdateGain(ResourcesPack gained)
+    {
+        //An object can't give several resources at the same time
+        //Thus, the result of the addition only returns the right resource number
+        int gainedNb = gained.food + gained.wood + gained.stone + gained.workForce + gained.motivation;
+
+        _gainText.text = "" + gainedNb;
+    }
+}
