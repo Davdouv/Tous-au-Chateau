e9dbd72caf1382fcd41b0cb17977149948d4ddad
Thyvong <kenthyvong.tan@gmail.com>
2019-03-18 16:47:06 +0100

    Update SpeechEvent_MapSelector_CreditEvent.cs

--- Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Selector/SpeechEvent_MapSelector_CreditEvent.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Selector/SpeechEvent_MapSelector_CreditEvent.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class SpeechEvent_MapSelector_CreditEvent : SpeechEvent {
 	private bool _hasOpenedAlready = false;
+    public bool hasPressed = false;
 
 	public override bool MustOpen() {
-				// Open automatically
-				if (!_hasOpenedAlready) {
-					_hasOpenedAlready = true;
-					return true;
-				}
-				return false;
+		// Open automatically
+		if (!_hasOpenedAlready && hasPressed) {
+			_hasOpenedAlready = true;
+			return true;
+		}
+		return false;
 	}
 
 	public override bool MustClose() {
+        hasPressed = false;
         return false;
 	}
 }
