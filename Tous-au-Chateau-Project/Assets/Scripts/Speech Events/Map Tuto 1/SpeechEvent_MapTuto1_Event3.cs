8d689ad58e9288e93b091383ef0a5be1e61833b4
Davdouv <d4vid.n@hotmail.fr>
2019-03-25 21:20:47 +0100

    Wait 2 seconds before closing speech event

--- Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Tuto 1/SpeechEvent_MapTuto1_Event3.cs
+++ Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map Tuto 1/SpeechEvent_MapTuto1_Event3.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class SpeechEvent_MapTuto1_Event3 : SpeechEvent {
 
     public GameObject ArrowToDisplay;
+    bool stopWait = false;
 
 	public override bool MustOpen() {
 		// Open after previous event is done
@@ -22,13 +23,21 @@ public class SpeechEvent_MapTuto1_Event3 : SpeechEvent {
 		return false;
 	}
 
-	public override bool MustClose() {
-		// When crushing the flickering tree. // Destroying the firstTree will set it to null
-    if (ResourceManager.Instance.GetWood() > 0)
+	public override bool MustClose()
     {
-        ArrowToDisplay.SetActive(false);
-        return true;
-    }
+		// When crushing the flickering tree. // Destroying the firstTree will set it to null
+        if (ResourceManager.Instance.GetWood() > 0)
+        {
+            StartCoroutine(WaitSeconds());
+            ArrowToDisplay.SetActive(false);            
+            return stopWait;
+        }
 		return false;
 	}
+
+    IEnumerator WaitSeconds()
+    {
+        yield return new WaitForSeconds(2);
+        stopWait = true;
+    }
 }
