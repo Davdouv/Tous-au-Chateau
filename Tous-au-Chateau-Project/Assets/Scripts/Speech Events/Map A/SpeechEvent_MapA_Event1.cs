fcea8e3b3237bf9c6561d1bcbec65a710f0d80bf
Davdouv <d4vid.n@hotmail.fr>
2019-03-26 13:55:49 +0100

    Create Map A

---
+++ Tous-au-Chateau-Project/Assets/Scripts/Speech Events/Map A/SpeechEvent_MapA_Event1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1 - INTRODUCTION
+
+public class SpeechEvent_MapA_Event1 : SpeechEvent {
+	private bool _hasOpenedAlready = false;
+
+	public override bool MustOpen() {
+        currentVillagersGroup.SetVillagersCanMove(false);
+
+		// Open automatically
+		if (!_hasOpenedAlready)
+        {
+			_hasOpenedAlready = true;
+			return true;
+		}
+		return false;
+	}
+
+    public override bool MustClose()
+    {
+        // When any action is done from the player (any key pressed)
+        // hasDoneAction = false;
+        if (hasDoneAction)
+        {
+            currentVillagersGroup.SetVillagersCanMove(true);
+        }
+        return hasDoneAction;
+    }
+}
